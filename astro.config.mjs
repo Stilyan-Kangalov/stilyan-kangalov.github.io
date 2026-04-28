10be0add6a31ff6b2b07695572daa181aebcac00
Stilyan Kangalov <stilyan.kangalov@gmail.com>
2026-04-29 02:29:39 +0300

    Changing the structure to fix the build failure.

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,8 +9,6 @@ export default defineConfig({
   site: "https://stilyan-kangalov.github.io",
   redirects: {
     "/blog": "/posts",
-    "/blog/category/[category]": "/archives",
-    "/blog/[...slug]": "/posts/[...slug]",
   },
   integrations: [sitemap(), icon(), mdx()],
   markdown: {
